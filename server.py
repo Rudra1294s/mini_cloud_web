f5e0eb59b53919933a44ae4fd1d9a6429e7f6f24
Rudra1294s <153521642+Rudra1294s@users.noreply.github.com>
2025-11-16 22:54:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -216,4 +216,4 @@ def download_chunk(file_id: int):
 # ================================================================
 if _name_ == "_main_":
     port = int(os.getenv("PORT", 5000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
